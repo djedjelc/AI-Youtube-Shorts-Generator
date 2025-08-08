63c568b6f532e04b7ae80c1a9714ab30ef457900
djedjelc <canddjedje@gmail.com>
2025-08-08 01:08:29 +0100

    use_po_token

--- Components/YoutubeDownloader.py
+++ Components/YoutubeDownloader.py
@@ -8,7 +8,7 @@ def get_video_size(stream):
 
 def download_youtube_video(url):
     try:
-        yt = YouTube(url)
+        yt = YouTube(url, use_po_token=True)
 
         video_streams = yt.streams.filter(type="video").order_by('resolution').desc()
         audio_stream = yt.streams.filter(only_audio=True).first()
